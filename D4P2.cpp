e91d92461185df071fb640096776cbefc63f25f7
Raj Agrawal <94883434+legitnewoob@users.noreply.github.com>
2023-12-04 22:41:12 +0530

    Create D4P2.cpp

---
+++ D4P2.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int parse_num(string s , int &i , int number = 0)
+{
+    while(i < s.size() and s[i] >= '0' and s[i] <= '9')
+    {
+        number = number * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return number;
+}
+
+void testcase() 
+{
+    string s;
+    int answer = 0 , count = 0;
+    map<int , int> card_count;
+    
+
+    while(getline(cin , s))
+    {
+        int n = s.size() , index = 0 , my_answer = 0;
+        map<int , int> map;
+
+        while(index < n and (s[index] < '0' or s[index] > '9')) index++;
+
+        int card_no = parse_num(s , index);
+
+        card_count[card_no]++;
+
+        while(index < n and (s[index] < '0' or s[index] > '9')) index++;
+
+  
+        for( ; s[index] != '|' ; index++)
+        {
+            if(s[index] >= '0' and s[index] <= '9')
+            {
+                int number = parse_num(s , index);
+
+                map[number]++;
+            }
+        }
+
+
+        for( ; index < n ; index++)
+        {
+            if(s[index] >= '0' and s[index] <= '9')
+            {
+                int number = parse_num(s , index);
+                if(map[number] != 0)
+                {
+                    my_answer++;
+                }
+            }
+        }
+
+        for(int i = card_no + 1 ; i <= 202 and i <= card_no + my_answer ; i++)
+        {
+            card_count[i] += card_count[card_no];
+        }
+    }
+
+    for(auto &[x , y] : card_count) answer += y;
+
+    cout << answer << endl;
+}
+
+
+int main() 
+{
+
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+        #endif
+
+    int tc = 1;
+    //cin >> tc;
+    while(tc--) testcase();
+
+    return 0;
+}
